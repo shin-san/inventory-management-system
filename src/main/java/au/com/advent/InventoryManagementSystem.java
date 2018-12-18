a20c2712182fcae817da92d7294ba3df060d9fef
JC <nchan009@gmail.com>
2018-12-18 23:27:26 +1000

    Initial Commit

---
+++ src/main/java/au/com/advent/InventoryManagementSystem.java
@@ -0,0 +1,13 @@
+package au.com.advent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InventoryManagementSystem {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InventoryManagementSystem.class);
+
+    public static void main(String[] args) {
+
+    }
+}
